da095000383e58029fc3b106eb5585d855ea43ef
Sergey Shepelev <temotor@gmail.com>
2010-09-05 02:05:57 +0400

    dummy version of kprintln (prints everything at top of screen)

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 // k411 main.c
 
+
+void kprintln(const char *s) {
+    unsigned short *mvideo = (unsigned short*) 0xb8000;
+    const char *ch = s;
+    for (int i = 0; *ch; ch++, i++) {
+        mvideo[i] = (unsigned char) *ch | 0x0700;
+    }
+}
+
 void kmain(void *mbd, unsigned int magic) {
     if ( magic != 0x2BADB002 ) {
         /* Something went not according to specs. Print an error */
@@ -12,18 +21,11 @@ void kmain(void *mbd, unsigned int magic) {
     /* or do your offsets yourself. The following is merely an example. */ 
     char *boot_loader_name = (char*) ((long*)mbd)[16];
 
-    char *msg = "Hello world!";
-    unsigned short *mvideo = (unsigned short*) 0xb8000;
-
     if (boot_loader_name) {
         // Just to use boot_loader_name
     }
 
-    int i;
-    char *ch;
-    for (i = 0, ch = msg; *ch; ch++, i++) {
-        mvideo[i] = (unsigned char) *ch | 0x0700;
-    }
+    kprintln("Hello world!");
 
     // TODO: scheduler, VM, etc
 }
