2ae303544c3385fe1db2cf793031eb7bc297947f
Sergey Shepelev <temotor@gmail.com>
2011-04-11 02:54:57 +0400

    Removed unused proc_regs

--- include/k411/core/task.h
+++ include/k411/core/task.h
@@ -3,10 +3,6 @@
 
 typedef void (* KThreadFun)();
 
-#ifdef ARCH_i386
-  #include <k411/hal/i386/proc_regs.h>
-#endif
-
 
 enum ProcessState {
 	INVALID_STATE = 0,
@@ -19,8 +15,6 @@ enum ProcessState {
 
 typedef struct SchedulerProcess_s {
 	enum ProcessState state;
-
-	struct proc_regs registers;
 
 	void *stack; // base of stack
 	void *esp;   // saved stack base so it can be destroyed when process is destroyed
