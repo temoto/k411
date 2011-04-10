b4f38e78ff5bcf543e304c49a99cf0c576d20e1d
Sergey Shepelev <temotor@gmail.com>
2011-04-10 22:39:46 +0400

    cosmetic: typedef 2 column formatting

--- include/stdint.h
+++ include/stdint.h
@@ -2,14 +2,14 @@
 // Copied from dux/metodo @ http://github.com/duckinator/dux
 #define STDINT_H
 
-typedef unsigned long long uint64_t;
-typedef unsigned int uint32_t;
-typedef unsigned short uint16_t;
-typedef unsigned char uint8_t;
+typedef unsigned long long  uint64_t;
+typedef unsigned int        uint32_t;
+typedef unsigned short      uint16_t;
+typedef unsigned char       uint8_t;
 
-typedef signed long long int64_t;
-typedef signed int int32_t;
-typedef signed short int16_t;
-typedef signed char int8_t;
+typedef signed long long    int64_t;
+typedef signed int          int32_t;
+typedef signed short        int16_t;
+typedef signed char         int8_t;
 
 #endif /* end of include guard: STDINT_H */
